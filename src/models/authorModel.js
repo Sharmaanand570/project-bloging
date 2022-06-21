b9cb5d5047327719480b7a937550b496a728871f
shivamd-96 <shivamd96@outlook.com>
2022-06-21 15:36:15 +0530

    models

---
+++ src/models/authorModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const authorSchema = new mongoose.Schema( {
+   fname: {type: String, required: true}, 
+   lname: {type : String , required :true},
+   title: {type : String, required :true,
+           enum : [ Mr, Mrs, Miss]}, 
+   email: { type: String, required:true,unique:true},
+   password : { type: Number , required: true},
+   isDeleted: { type : Boolean , default : false}
+   
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("author", authorSchema)
