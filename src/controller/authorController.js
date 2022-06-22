3423038993369e1449b2bffe2fcbe7e0aa1bb7b9
Anand Sharma <sharmaanand570@gmail.com>
2022-06-22 17:18:30 +0530

    authorController Update

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -3,22 +3,18 @@ const AuthorModel = require("../models/authorModel.js")
 const createAuthor = async function (req, res) {
     try {
         let authorData = req.body;
-        const { fname, lname, title, email, password } = authorData;
-        if (!(fname && lname && title && email && password)) {
-            res.status(400).send({ status: true, msg: "data not found" })
-        }
-        if ((typeof(fname)!==String) || (typeof(lname)!==String) || (typeof(title)!==String) || (typeof(email)!==String) || (typeof(password)!==String)) {
-            res.status(400).send({ status: true, msg: "please input value in string" })
-        }
-        let checkMail = await AuthorModel.findOne({ email: email })
+        let checkMail = await AuthorModel.findOne({ email:authorData.email})
         if (checkMail) {
             return res.status(400).send({ status: false, msg: " dupllicate email" })
         }
-        let savedAuthorData = await AuthorModel.create(authorData);
-        if (!savedAuthorData) {
-            res.status(400).send({ status: false, msg: "" })
+        try {
+            let savedAuthorData = await AuthorModel.create(authorData);
+            res.status(201).send({ status: true, data: savedAuthorData });
         }
-        res.status(201).send({ status: true, data: savedAuthorData });
+        catch (err) {
+            res.status(400).send({ status: false, msg: err.message })
+        }
+
     } catch (err) {
         res.status(500).send({ status: false, error: err.message })
     }
