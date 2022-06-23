0408cfef77cf7804bba7b08f466c33fce0ba9a57
Anand Sharma <sharmaanand570@gmail.com>
2022-06-23 18:15:07 +0530

    All API Done

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -6,11 +6,11 @@ const createAuthor = async function (req, res) {
         let authorData = req.body;
 
         const { fname, lname, title, email, password } = authorData;
-         
+
         if (!(fname && lname && title && email && password)) {
             return res.status(400).send({ status: false, msg: "key value is not present" })
         }
-    
+
         let checkMail = await authorModel.findOne({ email: email });
         if (checkMail) {
             return res.status(400).send({ status: false, msg: " duplicate email" })
@@ -19,10 +19,10 @@ const createAuthor = async function (req, res) {
             res.status(400).send({ status: false, msg: "please enter correct title eg Mr,Mrs,Miss" })
         }
 
-        if (typeof (fname) === "string" &&  fname.trim().length !== 0) {
-            if (typeof (lname) === "string"  &&  lname.trim().length !== 0) {
-                if (typeof (email) === "string"  &&  email.trim().length !== 0) {
-                    if (typeof (password) === "string"  &&  password.trim().length !== 0) {
+        if (typeof (fname) === "string" && fname.trim().length !== 0) {
+            if (typeof (lname) === "string" && lname.trim().length !== 0) {
+                if (typeof (email) === "string" && email.trim().length !== 0) {
+                    if (typeof (password) === "string" && password.trim().length !== 0) {
                         let savedAuthorData = await authorModel.create(authorData);
                         if (!savedAuthorData) {
                             res.status(400).send({ status: false, msg: "cannot create data" })
@@ -38,8 +38,31 @@ const createAuthor = async function (req, res) {
     }
 }
 
-const authorLogin = async function(req, res){
-
+const authorLogin = async function (req, res) {
+    try {
+        let userName = req.body.emailId;
+        let password = req.body.password;
+        let user = await authorModel.findOne({ emailId: userName, password: password });
+        if (!user)
+            return res.status(400).send({
+                status: false,
+                msg: "username or the password is not corerct",
+            })
+        let token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                admin: true,
+                group: 18,
+                radonProject: 3
+            },
+            "functionup-Project-1-Blogging-Room-18"
+        );
+        res.status(200).setHeader("x-auth-token", token);
+        res.status(200).send({ status: true, token: token });
+    }
+    catch (err) {
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
 }
 
 module.exports.authorLogin = authorLogin
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ let authenticate = async function (req, res, next) {
             res.status(404).send({ status: false, msg: "token must be present" })
         }
         else {
-            let decodedToken = jwt.verify(token, "functionUp-radon")
+            let decodedToken = jwt.verify(token, "functionup-Project-1-Blogging-Room-18")
             if (!decodedToken) {
                 res.status(400).send({ status: false, msg: "token is invalid" })
             }
@@ -28,7 +28,7 @@ let authorise = async function (req, res, next) {
         let authorIdQuery = req.query.authorId
         let authorIdBody = req.body.authorId
         let token = req.headers["x-auth-token"]
-        let decodedToken = jwt.verify(token, "functionUp-radon")
+        let decodedToken = jwt.verify(token, "functionup-Project-1-Blogging-Room-18")
         if (authorIdParams === decodedToken.authorId || authorIdQuery === decodedToken.authorId || authorIdBody === decodedToken.authorId) {
             next()
         }
